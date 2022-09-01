3969c65eec15e1b42355add1eb6182668c4af983
ishikawayutaissan <95219323+ishikawayutaissan@users.noreply.github.com>
2022-09-01 21:51:02 +0900

    Update sign-up.php
    Views　sign-up.php　13行目　HOME→ HOME_URLに変更

--- Views/sign-up.php
+++ Views/sign-up.php
@@ -10,7 +10,7 @@
 <body class="signup text-center">
     <main class="form-signup">
         <form action="sign-up.php" method="post">
-            <img src="<?php echo HOME;?>Views/img/logo-white.svg" alt="" class="logo-white">
+            <img src="<?php echo HOME_URL;?>Views/img/logo-white.svg" alt="" class="logo-white">
             <h1>アカウントを作る</h1>
             <input type="text" class="form-control" name="nickname" placeholder="ニックネーム" maxlength="50" required autofocus>
             <input type="text" class="form-control" name="name" placeholder="ユーザー名、例)techis132" maxlength="50" required>
